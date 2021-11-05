e4dec709e435064dff70deab90911472c6bac2d4
AbhinavDhakal <adhakalsir@gmail.com>
2021-11-05 21:48:04 +0545

    Add Finished

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ function addInList(list) {
 	if (!list) return;
 	let newTodo = document.createElement("div");
 	newTodo.classList.add("todo");
-	newTodo.innerHTML = `<div class="todoText">${list}</div> <button class="deleteBtn" onclick=destroyElement(this) >X</button>`;
+	newTodo.addEventListener("click", (e) => {
+		if (newTodo.classList.contains("finished")) {
+			newTodo.classList.remove("finished");
+		} else {
+			newTodo.classList.add("finished");
+		}
+	});
+	newTodo.innerHTML = `<div class="todoText">${list}</div> <button class="deleteBtn" onclick=destroyElement(this) style="text-decoration:none" >X</button>`;
 	lists.appendChild(newTodo);
 }
 
